1eaa998009031e58a4ad9b35915931ac356beb32
Pritam Nayak <Pritamnayak542003@gmail.com>
2026-07-26 20:04:04 +0530

    This is my First code

---
+++ FirstCode.py
@@ -0,0 +1,5 @@
+# This is a simple Python script that prints a greeting and the result of a basic arithmetic operation.
+
+print("Hello Python!")
+print(3+5)
+print("My Name Is Pritam Nayak", "age:", "=",25)
