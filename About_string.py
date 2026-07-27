2419b08e84e19bd0f33f657a95490f3b6e898f02
Pritam Nayak <Pritamnayak542003@gmail.com>
2026-07-27 20:06:55 +0530

    About String

---
+++ About_string.py
@@ -0,0 +1,27 @@
+
+# All are valid string declaration.
+
+str = "Hello World"
+str1 = 'Hello World!'
+str2 = """Hello World@"""
+print(str)
+print(str1)
+print(str2)
+
+# count the string length using len() function.
+str3 = '''Hello World'''
+len1 = len(str)
+print("length of String:",len1)
+
+# concatenate two strings using + operator.
+str4 = "Hello"
+str5 = "World"
+print("Show Concatenation of Two Strings:", str4 + " " + str5)
+
+# Slicing of string using index.
+str6 = "Hello World"
+print("Sliced String:", str6[1:4])
+print("string Sliced:", str6[:4])
+print("string sliced:", str6[2:])
+
+
