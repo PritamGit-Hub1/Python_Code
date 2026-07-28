6afadfe210de3522ee27b0d56e9bdcd4a21220a0
Pritam Nayak <Pritamnayak542003@gmail.com>
2026-07-28 07:23:30 +0530

    About Conditions

---
+++ About_Conditions.py
@@ -0,0 +1,12 @@
+# if - else if - else condition in python.
+
+light = input("Enter Your Trafic Light Color: ")
+if(light == "green"):
+    print("Go")
+elif(light == "yellow"):
+    print("Wait")
+elif(light == "red"):
+    print("Stop")
+else:
+    print("Invalid Light Color")
+    print ("End of program")
