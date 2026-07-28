9cde4d0b96787f35b54c440dac02e1c1cfe44335
Pritam Nayak <Pritamnayak542003@gmail.com>
2026-07-28 07:03:05 +0530

    Practice Questions

---
+++ Practice_Str.py
@@ -0,0 +1,8 @@
+# Write a program to input user's first name & print its length.
+
+firstname = input("Enter Your Name:")
+print("Your First Name Length is:", len(firstname))
+
+# WAP  to find occurrence of "$" in a string.
+str = "Hello$ Everyone$ Who Are$ You$?"
+print("Occurrence of $ in String:", str.count("$"))
